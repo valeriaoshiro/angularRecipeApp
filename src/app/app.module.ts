c8b6bd1ee905604665b5343b9746106c936b75c4
Valeria Oshiro <valeriaoshiro19@gmail.com>
2019-03-19 14:21:09 -0700

    Create recipes item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.
     HeaderComponent,
     RecipesComponent,
     RecipesListComponent,
-    RecipesDetailComponent
+    RecipesDetailComponent,
+    RecipesItemComponent
   ],
   imports: [
     BrowserModule
