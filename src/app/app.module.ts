e0da2a4e96ea63f62594ab4a215421dd09b373eb
Valeria Oshiro <valeriaoshiro19@gmail.com>
2019-03-19 14:20:22 -0700

    Create recipes detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipesListComponent
+    RecipesListComponent,
+    RecipesDetailComponent
   ],
   imports: [
     BrowserModule
